ea4e3405303a521fd7dd5f8527de538604ff3dcd
Greg J Preece <greg@preece.ca>
2020-12-14 22:39:08 -0800

    Adding "hello world" page to site part

---
+++ com_helloworld/site/src/Controller/DisplayController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace JohnSmith\Component\HelloWorld\Site\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Factory;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2020 John Smith. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * HelloWorld Component Controller
+ * @since  0.0.2
+ */
+class DisplayController extends BaseController {
+    
+    public function display($cachable = false, $urlparams = array()) {        
+        $document = Factory::getDocument();
+        $viewName = $this->input->getCmd('view', 'login');
+        $viewFormat = $document->getType();
+        
+        $view = $this->getView($viewName, $viewFormat);
+        
+        $view->document = $document;
+        $view->display();
+    }
+    
+}
---
+++ com_helloworld/site/src/View/Hello/HtmlView.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JohnSmith\Component\HelloWorld\Site\View\Hello;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2020 John Smith. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * View for the user identity validation form
+ */
+class HtmlView extends BaseHtmlView {
+    
+
+    /**
+     * Display the view
+     *
+     * @param   string  $template  The name of the layout file to parse.
+     * @return  void
+     */
+    public function display($template = null) {
+        // Call the parent display to display the layout file
+        parent::display($template);
+    }
+
+}
